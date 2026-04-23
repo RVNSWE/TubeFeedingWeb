c2ab8bad8079e982f8ce98db76f34e1a525fbd20
RVNSWE <monkey_lady@hotmail.com>
2026-04-23 16:44:10 +0100

    Separating input storage from calculations.

---
+++ TubeFeedingWeb/TubeFeedingWeb.Client/CalculateFeed.cs
@@ -0,0 +1,100 @@
+﻿namespace TubeFeedingWeb.Client
+{
+    /*
+     * Calculate food and water volumes for a specific day.
+     */
+    public class CalculateFeed
+    {
+        public double ContainersPerDay { get; set; }
+        public int MealsPerDay { get; set; }
+        public double MaxVolumePerMeal { get; set; }
+        public double TotalVolumePerMeal { get; set; }
+        public double FoodPerMeal { get; set; }
+        public double WaterPerMeal { get; set; }
+
+        private double totalVolumePerDay; // (ml)
+        private readonly float dayModifier;
+        private readonly UserInput input;
+
+        public CalculateFeed(UserInput userInput, int day)
+        {
+            input = userInput;
+            dayModifier = (1 / input.Days) * day; // Get starting RER fraction and multiply it by the current day number
+
+            CalculateFeedingPlan();
+        }
+
+        private void CalculateFeedingPlan()
+        {
+            double rER = (70 * Math.Pow(input.BodyWeight, 0.75)) * dayModifier; // Resting energy requirement (kcal) for the current day
+            double foodPerDay = rER / input.KcalPerG; // Total food per day (g)
+            ContainersPerDay = foodPerDay / input.DietNetWeight; // Estimated containers of food used up per day
+            double dietWaterVolume = foodPerDay * (input.DietWaterPercentage / 100); // Volume of water contained in food (ml)
+            MaxVolumePerMeal = input.BodyWeight * 20; // Max volume to be administered per meal (ml)
+            double waterPerDay = CalculateWaterPerDay(dietWaterVolume); // Additional water needed per day (ml)
+            totalVolumePerDay = foodPerDay + waterPerDay; // Estimated total volume administered per day (ml)
+            MealsPerDay = (int)Math.Round(totalVolumePerDay / MaxVolumePerMeal, 0, MidpointRounding.AwayFromZero); // Number of meals per day
+            TotalVolumePerMeal = totalVolumePerDay / MealsPerDay; // Estimated total volume administered per meal (ml)
+
+            while (TotalVolumePerMeal > MaxVolumePerMeal) // While the volume administered per meal exceeds the max allowable volume
+            {
+                MealsPerDay++; // Add another meal per day
+                TotalVolumePerMeal = totalVolumePerDay / MealsPerDay; // Recalculate the volume per meal
+
+                if (MealsPerDay > 23)
+                {
+                    break; // Do not exceed one meal per hour
+                }
+            }
+
+            FoodPerMeal = foodPerDay / MealsPerDay; // Food to administer per meal (g)
+            WaterPerMeal = (waterPerDay / MealsPerDay) - (input.FlushVolume * 2); // Extra water needed per meal (ml)
+
+            if (WaterPerMeal < 0)
+            {
+                WaterPerMeal = 0; // Water per meal cannot be negative
+            }
+
+            switch (input.BodyWeight) // Round volumes per meal to more appropriate SF
+            {
+                case < 10:
+                    FoodPerMeal = Math.Round(FoodPerMeal, 2, MidpointRounding.AwayFromZero);
+                    WaterPerMeal = Math.Round(WaterPerMeal, 2, MidpointRounding.AwayFromZero);
+                    break;
+                case < 20:
+                    FoodPerMeal = Math.Round(FoodPerMeal, 1, MidpointRounding.AwayFromZero);
+                    WaterPerMeal = Math.Round(WaterPerMeal, 1, MidpointRounding.AwayFromZero);
+                    break;
+                default:
+                    FoodPerMeal = Math.Round(FoodPerMeal, 0, MidpointRounding.AwayFromZero);
+                    WaterPerMeal = Math.Round(WaterPerMeal, 0, MidpointRounding.AwayFromZero);
+                    break;
+            }
+            ContainersPerDay = Math.Round(ContainersPerDay, 1, MidpointRounding.AwayFromZero);
+        }
+
+        /*
+         * Calculate the estimated daily fluid requirement based on species (true = cat, false = dog), and return the result.
+         */
+        private double CalculateWaterPerDay(double dietWaterVolume)
+        {
+            double waterPerDay;
+
+            if (input.Species == true)
+            {
+                waterPerDay = 80 * Math.Pow(input.BodyWeight, 0.75);
+            }
+            else
+            {
+                waterPerDay = 132 * Math.Pow(input.BodyWeight, 0.75);
+            }
+
+            if (dietWaterVolume > 0)
+            {
+                waterPerDay -= dietWaterVolume;
+            }
+
+            return waterPerDay;
+        }
+    }
+}
--- TubeFeedingWeb/TubeFeedingWeb.Client/UserInput.cs
+++ TubeFeedingWeb/TubeFeedingWeb.Client/UserInput.cs
@@ -1,5 +1,8 @@
 ﻿namespace TubeFeedingWeb.Client
 {
+    /*
+     * TO DO: Make editable
+     */
     public class UserInput
     {
         public double BodyWeight { get; set; } // (kg)
@@ -10,68 +13,20 @@
         public double KcalPerG { get; set; }
         public double DietNetWeight { get; set; } // (g)
         public double DietWaterPercentage { get; set; }
+        public double FlushVolume { get; set; }
 
-        private double totalVolumePerDay; // (ml)
-        private double totalVolumePerMeal; // (ml)
-        private int mealsPerDay;
-
-        public void CalculateFeedingPlan()
+        public UserInput()
         {
-            double rER = 70 * Math.Pow(BodyWeight, 0.75); // Resting energy requirement (kcal)
-            double foodPerDay = rER / KcalPerG; // Total food per day (g)
-            double containersPerDay = foodPerDay / DietNetWeight; // Estimated containers of food used up per day
-            double dietWaterVolume = foodPerDay * (DietWaterPercentage / 100); // Volume of water contained in food (ml)
-            double maxVolumePerMeal = BodyWeight * 20; // Max volume to be administered per meal (ml)
-            double flushVolume = CalculateFlushVolume(); // Volume of water to flush the tube with before and after each meal (ml)
-            double waterPerDay = CalculateWaterPerDay(dietWaterVolume); // Additional water needed per day (ml)
-            totalVolumePerDay = foodPerDay + waterPerDay; // Estimated total volume administered per day (ml)
-            mealsPerDay = (int)Math.Round(totalVolumePerDay / maxVolumePerMeal, 0, MidpointRounding.AwayFromZero); // Number of meals per day
-            totalVolumePerMeal = totalVolumePerDay / mealsPerDay; // Estimated total volume administered per meal (ml)
-
-            while (totalVolumePerMeal > maxVolumePerMeal) // While the volume administered per meal exceeds the max allowable volume
-            {
-                mealsPerDay ++; // Add another meal per day
-                totalVolumePerMeal = totalVolumePerDay / mealsPerDay; // Recalculate the volume per meal
-
-                if(mealsPerDay > 23)
-                {
-                    break; // Do not exceed one meal per hour
-                }
-            }
-
-            double foodPerMeal = foodPerDay / mealsPerDay; // Food to administer per meal (g)
-            double waterPerMeal = (waterPerDay / mealsPerDay) - (flushVolume * 2); // Extra water needed per meal (ml)
-
-            if(waterPerMeal < 0)
-            {
-                waterPerMeal = 0; // Water per meal cannot be negative
-            }
-
-            switch (BodyWeight) // Round volumes per meal to more appropriate SF
-            {
-                case < 10:
-                    foodPerMeal = Math.Round(foodPerMeal, 2, MidpointRounding.AwayFromZero);
-                    waterPerMeal = Math.Round(waterPerMeal, 2, MidpointRounding.AwayFromZero);
-                    break;
-                case < 20:
-                    foodPerMeal = Math.Round(foodPerMeal, 1, MidpointRounding.AwayFromZero);
-                    waterPerMeal = Math.Round(waterPerMeal, 1, MidpointRounding.AwayFromZero);
-                    break;
-                default:
-                    foodPerMeal = Math.Round(foodPerMeal, 0, MidpointRounding.AwayFromZero);
-                    waterPerMeal = Math.Round(waterPerMeal, 0, MidpointRounding.AwayFromZero);
-                    break;
-            }
-            containersPerDay = Math.Round(containersPerDay, 1, MidpointRounding.AwayFromZero);
+            CalculateFlushVolume();
         }
 
         /*
          * Calculate the volume of water to flush the feeding tube with before and after each feed.
          * If bodyweight is greater than value, check next value. Return first value lower than bodyweight.
          */
-        private double CalculateFlushVolume()
+        private void CalculateFlushVolume()
         {
-            double flushVolume = BodyWeight switch
+            FlushVolume = BodyWeight switch
             {
                 < 1.5 => 1,
                 < 2 => 1.5,
@@ -83,32 +38,6 @@
                 < 20 => 6,
                 _ => 10,
             };
-
-            return flushVolume;
-        }
-
-        /*
-         * Calculate the estimated daily fluid requirement based on species (true = cat, false = dog), and return the result.
-         */
-        private double CalculateWaterPerDay(double dietWaterVolume)
-        {
-            double waterPerDay;
-
-            if (Species == true)
-            {
-                waterPerDay = 80 * Math.Pow(BodyWeight, 0.75);
-            }
-            else
-            {
-                waterPerDay = 132 * Math.Pow(BodyWeight, 0.75);
-            }
-
-            if (dietWaterVolume > 0)
-            {
-                waterPerDay -= dietWaterVolume;
-            }
-
-            return waterPerDay;
         }
     }
 }
