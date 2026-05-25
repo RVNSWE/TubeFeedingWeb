c0c53a2396ee523c0d9472ffde73c8fdcbd6a68e
RVNSWE <monkey_lady@hotmail.com>
2026-05-25 23:14:24 +0100

    Added weight clinic calculator.

---
+++ TubeFeedingWeb/TubeFeedingWeb/WeightCalculations.cs
@@ -0,0 +1,48 @@
+﻿namespace TubeFeedingWeb
+{
+    public class WeightCalculations
+    {
+        public double WeightModifier { get; set; } = 1;
+        public double WeeklyPercentWeightLost { get; set; }
+        public double NextTargetWeight { get; set; }
+        public double IdealWeight { get; set; }
+
+        private double weightLost;
+        private double percentWeightLost;
+        private double onePercentWeightPerWeek;
+        private WeightClinicData data;
+
+        public WeightCalculations()
+        {
+            data = new();
+        }
+
+        public void Calculate()
+        {
+            weightLost = data.LastBodyWeight - data.CurrentBodyWeight;
+            percentWeightLost = weightLost / data.LastBodyWeight * 100;
+            WeeklyPercentWeightLost = percentWeightLost / data.WeeksSinceLastWeighed;
+            onePercentWeightPerWeek = 0.01 * data.CurrentBodyWeight * data.WeeksUntilNextWeighed;
+            NextTargetWeight = data.CurrentBodyWeight - onePercentWeightPerWeek;
+            IdealWeight = data.CurrentBodyWeight / WeightModifier;
+
+            WeeklyPercentWeightLost = Math.Round(WeeklyPercentWeightLost, 2, MidpointRounding.AwayFromZero);
+            NextTargetWeight = Math.Round(NextTargetWeight, 2, MidpointRounding.AwayFromZero);
+            IdealWeight = Math.Round(IdealWeight, 2, MidpointRounding.AwayFromZero);
+        }
+
+        public void UpdateData(WeightClinicData data)
+        {
+            this.data = data;
+
+            WeightModifier = data.BCS switch
+            {
+                6 => 1.1,
+                7 => 1.2,
+                8 => 1.3,
+                9 => 1.4,
+                _ => 1
+            };
+        }
+    }
+}
---
+++ TubeFeedingWeb/TubeFeedingWeb/WeightClinicData.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TubeFeedingWeb
+{
+    public class WeightClinicData
+    {
+        [Required(ErrorMessage = "A body condition score out of 9 must be entered."),
+            Range(6, 9, ErrorMessage = "Must be 6 or greater.")]
+        public int BCS { get; set; }
+        [Required(ErrorMessage = "The patient's body weight is required."),
+            Range(0.01, 200, ErrorMessage = "Please enter a valid body weight in kg, eg. 5.4.")]
+        public double CurrentBodyWeight { get; set; }
+        [Required(ErrorMessage = "The patient's body weight is required."),
+            Range(0.01, 200, ErrorMessage = "Please enter a valid body weight in kg, eg. 5.4.")]
+        public double LastBodyWeight { get; set; }
+        [Required(ErrorMessage = "Number of weeks since patient last weighed is required."),
+            Range(0.1, 52, ErrorMessage = "Please enter a number from 0.1 to 52.")]
+        public double WeeksSinceLastWeighed { get; set; }
+        [Required(ErrorMessage = "Number of weeks until next weight check is required."),
+            Range(0.1, 52, ErrorMessage = "Please enter a number from 0.1 to 52.")]
+        public double WeeksUntilNextWeighed { get; set; }
+    }
+}
