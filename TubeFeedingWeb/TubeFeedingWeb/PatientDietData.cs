061a854e5283ea03f23b852539727ba9681a5a0f
RVNSWE <monkey_lady@hotmail.com>
2026-07-13 22:15:29 +0100

    Fixed input validation for tube feeding form.

--- TubeFeedingWeb/TubeFeedingWeb/PatientDietData.cs
+++ TubeFeedingWeb/TubeFeedingWeb/PatientDietData.cs
@@ -8,21 +8,22 @@ namespace TubeFeedingWeb
         public string Species { get; set; } = string.Empty;
         [Required(ErrorMessage = "Please enter a body weight in kilograms."),
             Range(0.01, 200, ErrorMessage = "Please enter a valid body weight in kg, eg. 5.4.")]
-        public double BodyWeight { get; set; } = 5; // (kg)
-        [Range(1, 10, ErrorMessage = "Please enter a whole number from 1 to 10.")]
+        public double BodyWeight { get; set; } // (kg)
+        [Required(ErrorMessage = "Please enter a whole number."),
+            Range(1, 10, ErrorMessage = "Must be a whole number from 1 to 10.")]
         public int Days { get; set; } = 3;
         [RegularExpression(@"^[a-zA-Z0-9 ]+$", ErrorMessage = "Only alphanumeric characters are permitted (a-z and 0-9)."),
             MaxLength(100, ErrorMessage = "Length cannot exceed 100 characters.")]
-        public string? DietName { get; set; } = "Convalescence"; //string.Empty;
+        public string? DietName { get; set; } = string.Empty;
         [Required(ErrorMessage = "Please enter the kcal per gram (or per millilitre) of the diet to be fed."),
             Range(0.1, 10, ErrorMessage = "Must be kcal/g or kcal/ml.")]
-        public double KcalPerG { get; set; } = 1.128;
+        public double KcalPerG { get; set; }
         [Required(ErrorMessage = "Please enter the volume or weight of food per container in either grams or millilitres."),
             Range(1, 1000, ErrorMessage = "Please enter the total weight (g) or volume (ml) of food in each container. If using a powdered food, please enter the weight or volume after reconstitution.")]
-        public double DietNetWeight { get; set; } = 195; // (g)
+        public double DietNetWeight { get; set; } // (g)
         [Required(ErrorMessage = "Please enter the percentage moisture content of the diet."),
             Range(1, 99, ErrorMessage = "Only numbers from 1 - 99 are valid. If using a powdered food, please enter the percentage moisture content after reconstitution.")]
-        public double DietWaterPercentage { get; set; } = 77;
+        public double DietWaterPercentage { get; set; }
         public double FlushVolume { get; set; }
         [Required(ErrorMessage = "Please select whether water would be administered separately or mixed into the food.")]
         public string FoodDilutedOrSeparate { get; set; } = string.Empty;
